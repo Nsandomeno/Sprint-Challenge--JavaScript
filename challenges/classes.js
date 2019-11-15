1731873789404385f193a501c477c6130c61f62a
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2019-11-15 13:38:41 -0500

    M.V.P. done. Stetch done.

--- challenges/classes.js
+++ challenges/classes.js
@@ -15,6 +15,20 @@ class CuboidMaker{
 
 const cuboid = new CuboidMaker(4, 5, 5);
 
+class Cube extends CuboidMaker{
+    constructor(length, width, height){
+        super(length, width, height);
+    };
+    volume(){
+        return this.length * this.width * this.height;
+    };
+    surfaceArea(){
+        return 6 * (this.width * this.height);
+    };
+};
+
+const cube1 = new Cube(5,5,5);
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
